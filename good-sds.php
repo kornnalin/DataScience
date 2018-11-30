8eee4074fd5287f8a5d4b8798b051900ec99a43e
Kornnalin <32424977+kornnalin@users.noreply.github.com>
2018-11-30 20:58:22 +0700

    Add files via upload

---
+++ good-sds.php
@@ -0,0 +1,107 @@
+<?php 
+	include('header-sds.php');
+ ?>
+<br>
+ 	<div class="container-fliud">
+ 		<div class="row">
+ 			<div class="col-md-3 well">
+ 				<div class="sidenav w3-leftbar">
+ 					<h2 class="tab"> #การแสดงผล</h2>
+ 					<hr>
+ 					<a href="mainpag-sds.php"> 
+ 					  <img src="img/house (1).PNG"  width="40" height="40" >  Home
+ 					</a>
+	  				<a href="good-sds.php">
+	  					<img src="img/megaphone.PNG" class="img-cricle" width="40" height="40"> Good Promotion
+	  				</a>
+	  				<a href="bad-sds.php">
+	  					<img src="img/skull.PNG" class="img-cricle good-img" width="40" height="40"> Bad Promotion
+	  				</a>
+ 				</div>
+ 				
+ 			</div>
+
+<!--- content on right ---> 			
+ 			<div class="col-md-9">
+ 				<h1 class="show">- Good Promotion -</h1>
+ 				<br>
+ 				<div class="row">
+ 					<div class="col-md-4">
+ 						<img src="img/1NWLDaC4J23xmGTMSJ20obNfT0XkJo.jpg" class="img-responsive" width="100%" alt="Norway">
+						<div class="w3-container w3-card">
+						    <p class="des-img-good">ทาสไข่สุดฟิน ซื้อ 1 แถม 1</p>
+						</div>
+						<br>
+						<img src="img/1NWLDaC4J23xmGTMSJ20obNfT0XkJo.jpg" class="img-responsive" width="100%" alt="Norway">
+						<div class="w3-container w3-card">
+						    <p class="des-img-good">ทาสไข่สุดฟิน ซื้อ 1 แถม 1</p>
+						</div>
+						<br>
+						<img src="img/1NWLDaC4J23xmGTMSJ20obNfT0XkJo.jpg" class="img-responsive" width="100%" alt="Norway">
+						<div class="w3-container w3-card">
+						    <p class="des-img-good">ทาสไข่สุดฟิน ซื้อ 1 แถม 1</p>
+						</div>
+						<br>
+						<img src="img/1NWLDaC4J23xmGTMSJ20obNfT0XkJo.jpg" class="img-responsive" width="100%" alt="Norway">
+						<div class="w3-container w3-card">
+						    <p class="des-img-good">ทาสไข่สุดฟิน ซื้อ 1 แถม 1</p>
+						</div>
+				<br>
+ 					</div>
+ 					<div class="col-md-4">
+ 						<img src="img/1NWLDaC4J23xmGTMSJ20obNfT0XkJo.jpg" class="img-responsive" width="100%" alt="Norway">
+						<div class="w3-container w3-card">
+						    <p class="des-img-good">ทาสไข่สุดฟิน ซื้อ 1 แถม 1</p>
+						</div>
+						<br>
+						<img src="img/1NWLDaC4J23xmGTMSJ20obNfT0XkJo.jpg" class="img-responsive" width="100%" alt="Norway">
+						<div class="w3-container w3-card">
+						    <p class="des-img-good">ทาสไข่สุดฟิน ซื้อ 1 แถม 1</p>
+						</div>
+						<br>
+						<img src="img/1NWLDaC4J23xmGTMSJ20obNfT0XkJo.jpg" class="img-responsive" width="100%" alt="Norway">
+						<div class="w3-container w3-card">
+						    <p class="des-img-good">ทาสไข่สุดฟิน ซื้อ 1 แถม 1</p>
+						</div>
+						<br>
+						<img src="img/1NWLDaC4J23xmGTMSJ20obNfT0XkJo.jpg" class="img-responsive" width="100%" alt="Norway">
+						<div class="w3-container w3-card">
+						    <p class="des-img-good">ทาสไข่สุดฟิน ซื้อ 1 แถม 1</p>
+						</div>
+				<br>
+ 					</div>
+ 					<div class="col-md-4">
+ 						<img src="img/1NWLDaC4J23xmGTMSJ20obNfT0XkJo.jpg" class="img-responsive" width="100%" alt="Norway">
+						<div class="w3-container w3-card">
+						    <p class="des-img-good">ทาสไข่สุดฟิน ซื้อ 1 แถม 1</p>
+						</div>
+						<br>
+						<img src="img/1NWLDaC4J23xmGTMSJ20obNfT0XkJo.jpg" class="img-responsive" width="100%" alt="Norway">
+						<div class="w3-container w3-card">
+						    <p class="des-img-good">ทาสไข่สุดฟิน ซื้อ 1 แถม 1</p>
+						</div>
+						<br>
+						<img src="img/1NWLDaC4J23xmGTMSJ20obNfT0XkJo.jpg" class="img-responsive" width="100%" alt="Norway">
+						<div class="w3-container w3-card">
+						    <p class="des-img-good">ทาสไข่สุดฟิน ซื้อ 1 แถม 1</p>
+						</div>
+						<br>
+						<img src="img/1NWLDaC4J23xmGTMSJ20obNfT0XkJo.jpg" class="img-responsive" width="100%" alt="Norway">
+						<div class="w3-container w3-card">
+						    <p class="des-img-good">ทาสไข่สุดฟิน ซื้อ 1 แถม 1</p>
+						</div>
+				<br>
+ 					</div>
+ 				</div>
+ 			</div>
+ 		</div>
+
+<!--- img-good ---> 		
+ 		
+ 		
+ 	</div>
+
+
+<?php 
+	include('footer-sds.php');
+?>
